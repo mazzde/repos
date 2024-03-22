b97432269c012c145407c7691f133d5cd8c5fb32
Mazza <164398708+mazzde@users.noreply.github.com>
2024-03-22 02:34:33 +0000

    Push
    Dice game to py branch

--- PythonDiceGame1/PythonDiceGame1/PythonDiceGame1.py
+++ PythonDiceGame1/PythonDiceGame1/PythonDiceGame1.py
@@ -56,3 +56,5 @@ winnning_idx = player_scores.index(max_score)
 print("Player number", winning_idx + 1,
       "is the winner with a score of:", max_score)
 
+#add after each player has 3 breaks show current highest score.
+
